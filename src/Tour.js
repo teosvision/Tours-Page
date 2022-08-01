8de0d8903ad1ef1d6b8f42dce9e502caf6d6b1e1
teosvision <mateovelo99@gmail.com>
2022-08-01 18:10:52 +0200

    first commit

---
+++ src/Tour.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image,info,price,name,removeTour}) => {
+ 
+  const [readMore,setreadMore]=useState(false);
+ 
+  return (
+
+   
+     <article className='single-tour'>
+      <img src={image} alt='name'></img>
+      <footer>
+        <div className='tour-info'>
+          <h4>
+            {name}
+          </h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>
+          {readMore?info: `${info.substring(0,200)}...`}
+          <button onClick={()=> setreadMore(!readMore) }>
+            {readMore?'show less':'read more'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={ () => removeTour(id)}> delete this </button>
+      </footer>
+     </article>
+     
+     
+     
+
+)
+};
+export default Tour;
---
+++ src/Tours.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours,removeTour}) => {
+  return <section>
+    <div className='tittle'>
+      <h1>our tours</h1>
+      <div className='underline'></div>
+
+    </div>
+    <div>
+      {tours.map((tour)=>{
+        return <Tour key={tour.id}{...tour} removeTour={removeTour}>
+
+         </Tour>
+      })}
+    </div>
+  </section>
+};
+
+
+export default Tours;
